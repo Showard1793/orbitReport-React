03be71e4add414072862155b6c66853cb5736d26
Stephen Howard <Showard1793@gmail.com>
2024-08-24 13:27:09 -0500

    Update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import "./styling.css";
 
 export default function Banner() {
   return (
-    <div>
+    <div className='h1'>
       <header>Orbit Report</header>
         <p>
         Click on the buttons to see the satellites in that orbit type
